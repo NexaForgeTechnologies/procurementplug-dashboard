5bf37d4974a1c7792db9f0da3c0f481166cbebbb
unknown <anasmughal595@gmail.com>
2025-10-01 02:02:19 +0500

    add actions

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+  reactStrictMode: true, 
+  output: 'standalone',   // ✅ this is required
+  
 };
 
 export default nextConfig;
