35ee7fb71a4d3a68f331506e6c4ed1cc62711997
NexaForgeTechnologies <project@nexaforgetech.com>
2025-10-02 17:43:07 +0500

    Update page.tsx
    Admin panel text added for dashboard

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -5,7 +5,13 @@ function page() {
 
     <>
       <div className="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700">
-        Dashboard
+        <h1 className="text-2xl font-bold mb-2">Welcome to The Admin Dashboard!</h1>
+        <h2 className="text-xl font-semibold mb-4">The Procurement Plug
+Your Ultimate Ecosystem for Procurement Excellence</h2>
+        <p>
+          Manage your ecosystem with ease. From here, you can create, update, and monitor all key modules — Business Hub, Community Hub, Personal Development Hub, and more.
+Stay in control of content, events, users, and workflows to ensure everything runs smoothly. Your actions here power the entire Procurement Plug experience for professionals and businesses.
+        </p>
       </div>
     </>
 
