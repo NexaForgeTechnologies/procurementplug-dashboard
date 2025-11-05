c6db98182dc43d7d783c2e3619494cfcc94309a6
HasnainWebWizard <hasnainijazm321@gmail.com>
2025-11-05 16:55:38 +0500

    Again

--- src/lib/sqldb.ts
+++ src/lib/sqldb.ts
@@ -3,22 +3,22 @@ import mysql from "mysql2/promise";
 
 // Read env variables
 const {
-  DB_HOST,
-  DB_USER,
+  DATABASE_HOST,
+  DATABASE_USER,
   DB_PASSWORD,
-  DB_NAME
+  DATABASE_NAME
 } = process.env;
 
-if (!DB_HOST || !DB_USER || !DB_NAME) {
+if (!DATABASE_HOST || !DATABASE_USER || !DATABASE_NAME) {
   throw new Error("Database environment variables are missing!");
 }
 
 // Create a connection pool
 export const sqldb = mysql.createPool({
-  host: DB_HOST,
-  user: DB_USER,
+  host: DATABASE_HOST,
+  user: DATABASE_USER,
   password: DB_PASSWORD || "",
-  database: DB_NAME,
+  database: DATABASE_NAME,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
