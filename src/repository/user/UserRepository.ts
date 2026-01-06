b9c9859ef30e8de2db603719bb46737d0c52c4c1
AhmadFiaz-1 <h.ahmad.fiaz@gmail.com>
2026-01-06 14:26:30 +0500

    testing login issue

--- src/repository/user/UserRepository.ts
+++ src/repository/user/UserRepository.ts
@@ -6,10 +6,17 @@ export class UserRepository {
 
   static async getUserByEmail(email: string): Promise<UsersDM | null> {
     try {
-      const [rows] = await db.query<RowDataPacket[]>(
+      // const [rows] = await db.query<RowDataPacket[]>(
+      //   "SELECT * FROM users WHERE email = ?",
+      //   [email]
+      // );
+      await db.query("SELECT 1");
+
+      const [rows] = await db.execute<RowDataPacket[]>(
         "SELECT * FROM users WHERE email = ?",
         [email]
       );
+
       if (rows.length > 0) {
         const row = rows[0];
         return {
